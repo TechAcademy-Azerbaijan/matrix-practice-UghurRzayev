a2f10949012c6e704ac19a8ce25c6d83f96da190
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-06 10:45:58 +0400

    Task 1 done

--- task-1/main.js
+++ task-1/main.js
@@ -5,4 +5,15 @@ let matrix = [
     [0, 1, -3]
 ]
 
-// Write code here. 
+let count = 0
+let sum = 0
+for (let i = 0; i < matrix.length; i++) {
+    const element = matrix[i];
+    for (let j = 0; j < element.length; j++) {
+        const element2 = element[j];
+        if (element2<0 && element2%2==0) {
+            count += 1
+            sum += element2
+        }
+    }
+} console.log(`${count}\n${sum}`);
