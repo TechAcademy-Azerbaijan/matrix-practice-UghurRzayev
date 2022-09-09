33c156efa7bee0c66d3ee225d9a7d58a8c98ceec
Ugur Rzayev <ughurrzayev@gmail.com>
2022-09-09 11:40:30 +0400

    Task 3 done

--- task-3/main.js
+++ task-3/main.js
@@ -7,4 +7,24 @@ let matrix = [
     [4, 5, 6, 7, 7]
 ]
 
-// Write code here. 
+let min = matrix[0][0];
+
+for (let i = 0; i < matrix.length; i++) {
+  let arr = matrix[i];
+  for (let j = 0; j < arr.length; j++) {
+    const element = arr[j];
+    if (element < min) {
+      min = element;
+    }
+  }
+}
+
+for (let i = 0; i < matrix.length; i++) {
+  let arr = matrix[i];
+  for (let j = 0; j < arr.length; j++) {
+    const element = arr[j];
+    if (element === min) {
+      console.log(i+1);
+    }
+}
+}
